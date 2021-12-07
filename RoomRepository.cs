87498f0bef1499873cc051746718c0a94834e5ca
scrawleyy1 <crawley.sydney@gmail.com>
2021-12-07 07:41:53 -0800

    first commit - complete #1

---
+++ RoomRepository.cs
@@ -0,0 +1,11 @@
+﻿using Roommates.Repositories;
+
+namespace Roommates
+{
+    internal class RoomRepository : ChoreRepository
+    {
+        public RoomRepository(string connectionString) : base(connectionString)
+        {
+        }
+    }
+}
